a4089eed6f81908760a97118756905f9e7ff6539
Yosigoon <yosigoon@gmail.com>
2017-07-16 23:56:49 +0900

    7/16

---
+++ Array.java
@@ -0,0 +1,11 @@
+package Test;
+
+public class Array {
+	public static void main(String[] args) {
+		int[][] a = { { 10, 20 }, { 30, 40 }, { 50, 60 } };
+
+		System.out.println("a[0][0]=" + a[0][0] + " a[0][1]=" + a[0][1]);
+		System.out.println("a[1][0]=" + a[1][0] + " a[1][1]=" + a[1][1]);
+		System.out.println("a[2][0]=" + a[2][0] + " a[2][1]=" + a[2][1]);
+	}
+}
